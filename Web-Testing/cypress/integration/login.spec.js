f440259861b85ab68624a91b3b130542f9c00d41
Ahmed-Emad10 <65132004+Ahmed-Emad10@users.noreply.github.com>
2021-11-10 19:14:51 +0200

    inyial login page

---
+++ Web-Testing/cypress/integration/login.spec.js
@@ -0,0 +1,146 @@
+describe('login',()=>{
+
+    beforeEach(()=>{
+        cy
+          .visit('https://www.tumblr.com/login');
+    })
+
+    it('empty email and empty password',()=>{
+        
+        cy
+          .get('form > .TRX6J > .EvhBA')
+          .click();
+       
+        cy
+          .get('.oFCPF')
+          .should('have.text','You do have to fill this stuff out, you know.');
+    })
+
+    it('empty email and incorrect password',()=>{
+        cy
+          .get('[name=password]')
+          .type('asdfghj');
+
+        cy
+          .get('form > .TRX6J > .EvhBA')
+          .click();
+       
+        cy
+          .get('.oFCPF')
+          .should('have.text','You forgot to enter your email!');
+    })
+
+    it('empty email and correct password',()=>{
+        cy
+          .get('[name=password]')
+          .type('y35SYi:7@+dK7J');
+
+        cy
+          .get('form > .TRX6J > .EvhBA')
+          .click();
+       
+        cy
+          .get('.oFCPF')
+          .should('have.text','You forgot to enter your email!');
+    })
+
+    it('non existing email and empty password',()=>{
+        cy
+          .get('[name=email]')
+          .type('ahmedmad@gmail.com');
+
+        cy
+          .get('form > .TRX6J > .EvhBA')
+          .click();
+       
+        cy
+          .get('.oFCPF')
+          .should('have.text','You forgot to enter your password!');
+    })
+
+    it('existing email and empty password',()=>{
+        cy
+          .get('[name=email]')
+          .type('itztysocfkegfkgoij@adfskj.com');
+
+        cy
+          .get('form > .TRX6J > .EvhBA')
+          .click();
+       
+        cy
+          .get('.oFCPF')
+          .should('have.text','You forgot to enter your password!');
+    })
+
+    it('non existing email and incorrect password',()=>{
+        cy
+          .get('[name=email]')
+          .type('ahmedemad@gmail.com');
+
+        cy
+          .get('[name=password]')
+          .type('asdfghj');
+        
+        cy
+          .get('form > .TRX6J > .EvhBA')
+          .click();
+       
+        cy
+          .get('.oFCPF')
+          .should('have.text','Your email or password were incorrect.');
+    })
+
+    it('non existing email and correct password',()=>{
+        cy
+          .get('[name=email]')
+          .type('ahmedmad@gmail.com');
+
+        cy
+          .get('[name=password]')
+          .type('y35SYi:7@+dK7J');
+        
+        cy
+          .get('form > .TRX6J > .EvhBA')
+          .click();
+       
+        cy
+          .get('.oFCPF')
+          .should('have.text','Your email or password were incorrect.');
+    })
+
+    it('existing email and incorrect password',()=>{
+        cy
+          .get('[name=email]')
+          .type('itztysocfkegfkgoij@adfskj.com');
+
+        cy
+          .get('[name=password]')
+          .type('assdfghjkltr');
+        
+        cy
+          .get('form > .TRX6J > .EvhBA')
+          .click();
+       
+        cy
+          .get('.oFCPF')
+          .should('have.text','Your email or password were incorrect.');
+    })
+    
+    it('existing email and correct password',()=>{
+        cy
+          .get('[name=email]')
+          .type('itztysocfkegfkgoij@adfskj.com');
+
+        cy
+          .get('[name=password]')
+          .type('y35SYi:7@+dK7J');
+        
+        cy
+          .get('form > .TRX6J > .EvhBA')
+          .click();
+       
+        cy
+          .url()
+          .should('include', 'dashboard');
+    })
+})
