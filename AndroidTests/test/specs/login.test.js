9e0160ee6d5d1b60c160c4962cb3cee462bcd0e7
Ahmed-Emad10 <65132004+Ahmed-Emad10@users.noreply.github.com>
2021-12-29 19:28:32 +0200

    appium style

--- AndroidTests/test/specs/login.test.js
+++ AndroidTests/test/specs/login.test.js
@@ -7,36 +7,25 @@ const LoginPOM = require ("../pageobjects/login.page.js");
 
 
 describe('ًlogin tests', ()=>{
-    const logInBtn = await LoginPOM.loginButton();
+   /* const logInBtn = await LoginPOM.loginButton();
     const logInEmail = await LoginPOM.loginWithEmailButton();
     const emailField = await LoginPOM.emailTextBox();
     const passwordField = await LoginPOM.passwordTextBox();
-    const loginbtn = await LoginPOM.continueButton().getAttribute("enabled");
-    it.only('no email no password', ()=>{
-        driver.setTimeouts(5000)
-        logInBtn.click();
-        logInEmail.click();
-        await emailField.addValue("");
-        await passwordField.addValue(""); 
-        expect(loginbtn).toBe("false"); 
+    const loginbtn = await LoginPOM.continueButton().getAttribute("enabled");*/
+    it.only('no email no password', async()=>{
+        
+        await (await LoginPOM.loginButton()).click();
+        await driver.pause(2000)
+        await (await LoginPOM.loginWithEmailButton()).click();
+        driver.pause(2000)
+        await (await LoginPOM.emailTextBox()).addValue("");
+        driver.pause(2000)
+        await (await LoginPOM.passwordTextBox()).addValue(""); 
+        driver.pause(2000)
+        await (await LoginPOM.continueButton()).getAttribute("enabled").then((result)=>{
+            expect(result).equal("false"); 
+        })
+        
     })
 
 })
-
-
-/*module.exports.EnterEmailAndPassword = async (inputEmail = '', inputPassword = '') => {
-    // inputEmail = inputEmail === ''? 'email':inputEmail;
-    // inputPassword = inputPassword === ''? 'password':inputPassword;
-    const EmailField = await LoginPOM.emailFieldPOM();
-    await stateAssertion(EmailField);
-    await EmailField.addValue(inputEmail);
-    const email = await EmailField.getText();
-    expect(email).toBe(inputEmail === '' ? 'email' : inputEmail);
-
-    const PasswordField = await LoginPOM.passwordFieldPOM();
-    await stateAssertion(PasswordField);
-    await PasswordField.addValue(inputPassword);
-    const password = await PasswordField.getText();
-    expect(password).toBe(inputPassword === '' ? 'password' : CreatePasswordDots(password));
-    expect(password).not.toBe(inputPassword);
-}*/
